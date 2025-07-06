e5b6ae2981fe9a27dfeb4ff949cbe7f74c48a7bc
Imran Zia <imranzia103@gmail.com>
2025-07-07 00:01:06 +0500

    Add files via upload
    payment method

---
+++ redux/store.js
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
+import authReducer from './features/auth/authSlice.js';
+import { apiSlice } from "./Api/apiSlice.js";
+
+
+
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath] : apiSlice.reducer,
+    auth: authReducer
+  },
+    
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true
+});
+
+setupListeners(store.dispatch);
+export default store;
